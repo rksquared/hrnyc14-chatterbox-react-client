2dc29af252cd4bcf391f1f4928f4b81316d862e4
rksquared <rahul@rahulkrish.com>
2018-03-28 00:38:03 -0400

    got modal sign on working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import Modal from 'react-modal';
 import './styles/styles.css';
 
 import RoomSelector from './components/roomSelector.js';
 import ChatRoom from './components/chatRoom.js';
 import ChatInput from './components/chatInput.js';
 
+
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'rk',
+      modalIsOpen: false,
+      username: '',
       roomname: 'lobby',
       messages: [],
       newMessage: ''
@@ -19,7 +38,12 @@ class App extends Component {
     this.handleChatSubmission = this.handleChatSubmission.bind(this);
     this.fetchChats = this.fetchChats.bind(this);
     this.postChats = this.postChats.bind(this);
-
+  
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleUserNameInput = this.handleUserNameInput.bind(this);
+      
   }
 
   componentDidMount() {
@@ -35,9 +59,41 @@ class App extends Component {
     // this.setState({
     //   username: window.location.search.substr(10)
     // });
+    if (this.state.username === '') {
+      this.setState({
+        modalIsOpen: true
+      });
+
+    }
+
     this.fetchChats();
     
   }
+
+  //modal
+
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  handleUserNameInput(ev) {
+    this.setState({
+     username: ev.target.value
+    });
+    console.log('username', this.state.username);
+  }
+
+  //end modal
 
   fetchChats() {
     Axios.get('http://127.0.0.1:3000/classes/messages/')
@@ -71,12 +127,13 @@ class App extends Component {
     console.log(this.state.newMessage);
   }
 
-  handleChatSubmission() {
+  handleChatSubmission(ev) {
     //post with this.state.newMessage
     let message = {
       username: this.state.username,
       text: this.state.newMessage
     }
+    ev.preventDefault();
     this.postChats(message);
   }
 
@@ -87,6 +144,36 @@ class App extends Component {
 
     return (
       <div>
+        <div>
+          <button onClick={this.openModal}>Open Modal</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+
+            <h2 ref={subtitle => this.subtitle = subtitle}>Hi There</h2>
+            <div>Sign In to Chatterbox!</div>
+            <form onSubmit={(ev) => {
+              ev.preventDefault();
+              this.closeModal()}}
+            >
+              <input 
+                type="text"
+                value={this.state.username}
+                onChange={this.handleUserNameInput}
+              />
+              <input 
+                type="submit" 
+                name="submit" 
+                className="submit" 
+              />
+            </form>
+            <button onClick={this.closeModal}>close</button>
+          </Modal>
+        </div>
         <div id="main">
           <h1>chatterbox</h1>
           <button onClick={this.fetchChats}>test get</button>
